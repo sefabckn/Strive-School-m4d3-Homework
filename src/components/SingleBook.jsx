6ea25a1d5c832a1b72032f845d741f734a0b0976
SefaBckn <31974256+sefabckn@users.noreply.github.com>
2021-12-15 17:03:17 +0100

    ex 6-7 solved

--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -1,21 +1,32 @@
 import { Container, Row } from 'react-bootstrap'
 import Card from 'react-bootstrap/Card'
-
-const SingleBook = (props) =>{
-    return(
-        <Container>
-            <Row>{
-                    <Card style={{ width: '12rem' }}>
-                        <Card.Img variant="top" src={props.book.img}/>
-                        <Card.Body>
-                            <Card.Title>{props.book.category}</Card.Title>
-                            <Card.Text>
-                            {props.book.title}
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>        
-               
-                }</Row>
-        </Container>
-    )}
+import { Component } from 'react'
+class SingleBook extends Component{
+    state= {
+        selected: false
+    }
+    render(){
+        return(
+            <Container>
+                <Row>{
+                        <Card style={{ width: '12rem' }}>
+                            <Card.Img 
+                            variant="top" 
+                            src={this.props.book.img}
+                            onClick={()=> {this.setState({selected:true})}}
+                            style={{border:`2px solid ${this.state.selected?"red":"none"}`}}
+                            />
+                            <Card.Body>
+                                <Card.Title>{this.props.book.category}</Card.Title>
+                                <Card.Text>
+                                {this.props.book.title}
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>        
+                   
+                    }</Row>
+            </Container>
+        )
+    }
+   }
 export default SingleBook
